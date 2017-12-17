358d2806f1e2c02d0a2ceb4096640838177f1a5e
HoangPhan <huyhoangprojects@gmail.com>
2017-12-17 15:20:07 +0700

    Unit 12: State

--- public/index.js
+++ public/index.js
@@ -6,13 +6,21 @@ var MyComponent = React.createClass({
 	getAttitude: function() {
 		alert(this.props.children);
 	},
+	getInitialState: function(){
+		return {total: this.props.total};
+	},
+	addStudent: function() {
+		this.setState({total: parseInt(this.state.total) + 1});
+	},
 	render: function(){
 		return (
 			<div>
 				<h1> Hello {this.props.name} </h1>
 				<h2> I {this.props.children} {this.props.name} </h2>
+				<p>Total: {this.state.total}</p>
 				<MyChildComponent name={this.props.name} />
 				<button onClick={() => getName(this.props.children)}>Get</button>
+				<button onClick={this.addStudent}>Add</button>
 			</div>
 		);
 	}
@@ -27,9 +35,9 @@ var MyChildComponent = React.createClass({
 
 ReactDOM.render (
 	<div>
-		<MyComponent name ="ReactJS">Love </MyComponent>
+		<MyComponent name ="ReactJS" total="5">Love </MyComponent>
 		<hr />
-		<MyComponent name ="NodeJS"> Hate </MyComponent>
+		<MyComponent name ="NodeJS" total="15"> Hate </MyComponent>
 	</div>,
 	document.getElementById("root")
 );
